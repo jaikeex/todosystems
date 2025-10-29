9935ec72d607b0c7d795620626e9fe6b09470841
Jakub Hrubý <hrubyy.jakub@gmail.com>
2025-10-29 07:23:17 +0100

    real github link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ export const ERROR_DISPLAY_DURATION = 5000;
 export const MAX_TASK_TEXT_LENGTH = 100;
 export const SKELETON_ITEMS_COUNT = 3;
 
-export const GITHUB_URL = 'https://github.com/jaikeex';
+export const GITHUB_URL = 'https://github.com/jaikeex/todosystems';
 export const APP_NAME = 'TODOSYSTEMS';
 
 export const FILTERS: Filter[] = ['all', 'active', 'done'];
